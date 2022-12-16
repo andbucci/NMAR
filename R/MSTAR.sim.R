78839faa179db161fedc719872960b63c215acb7
andbucci <andbucci88@gmail.com>
2022-12-16 10:03:22 +0100

    Added simulation from MSTAR

--- R/MSTAR.sim.R
+++ R/MSTAR.sim.R
@@ -10,7 +10,6 @@
 #' @param p number of lags
 #' @param regimes number of regimes
 #' @param nsim number of simulated data
-#' @param burnin number of data to be burned
 #' @param constant \code{TRUE} or \code{FALSE} to include or not a constant matrix.
 #' @param A \code{list} of values for the row-wise coefficient \code{(m×m)} matrices, one list for each lag containing a list of matrices for each regime.
 #' @param B \code{list} of values for the column-wise coefficient \code{(n×n)} matrices, one list for each lag containing a list of matrices for each regime.
